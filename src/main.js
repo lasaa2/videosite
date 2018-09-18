e124ceacf9c4845eb3bcd532af8b731a2970d2a2
lasaa2 <musa.laspe@gmail.com>
2018-09-18 12:31:36 +0300

    pure videojs

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue' // tuodaan käytettäväksi vue kirjastoa
-// import App from './App' // tuodaan App.vue tiedosto käytettäväksi
 
-import VueVideoPlayer from 'vue-video-player' // tuodaan vue-video-player kirjasto
-import 'vue-video-player/src/custom-theme.css' // tuodaan vue-video-player tyylitiedosto
+import videojs from 'video.js'
 import 'video.js/dist/video-js.css' // tuodaan videojs tyylitiedosto
+import './styles.css'
 
-Vue.config.productionTip = false
-Vue.use(VueVideoPlayer) // use global vuevideoplayer plugin
-
-/* eslint-disable no-new */
-/* 
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>',
-})
-
-*/
+window.videojs = require('video.js');
 
 /* jos jsonin hakee tiedostosta value arvo filtterissä on yksi objekti eikä monta objektia, miksi? */
 // import json from './data.json';
@@ -27,37 +15,54 @@ new Vue({
 
 Vue.component('videoplayer-component', {
   props: ['videos'],
-  data () {
+  data: function() {
     return {
-        url: '',
-        playerOptions: { 
-            autoplay: false,
-            controls: true,
-            muted: true,
-            sources: [{
-                type: "video/mp4",
-                src: "http://vjs.zencdn.net/v/oceans.mp4"
-            }],
-            poster: "https://oubs.fi/wp-content/uploads/2016/11/cropped-oubs_white_teksti_lapi_152px.png"
-        }
+      player: '',
+      playerOptions: {
+        autoplay: false,
+        controls: true,
+        muted: true,
+        height: 360,
+        width: 640,
+        poster: "https://oubs.fi/wp-content/uploads/2016/11/cropped-oubs_white_teksti_lapi_152px.png"
+      }
     }
   },
-  methods: {
-   playerSetSrc: function(url) {
-      this.playerOptions.sources[0].src = url;
-    }
-  },
-  template: `<div>
-      <playlist-component @clicked="playerSetSrc" v-bind:videos="videos"/>
-      <video-player 
-      class="vjs-custom-skin" 
-      ref="videoPlayer" 
-      :options="playerOptions">
-      </video-player>
-  </div>
-  `
-});
 
+  methods: {
+    playerInit() {
+      this.player = videojs('player', this.playerOptions);
+    },
+
+    playerSetUrl(url) {
+      this.player.src(url);
+    }
+
+  },
+
+  mounted() {
+    this.playerInit();
+    this.playerSetUrl(this.videos[0].url);
+  },
+
+  template: `
+  <div class="videowindow">  
+      <video
+      playsinline  
+          id="player"
+            class="video-js videoplayer">
+          <p class="vjs-no-js">
+            To view this video please enable JavaScript, and consider upgrading to a
+            web browser that
+            <a href="http://videojs.com/html5-video-support/" target="_blank">
+              supports HTML5 video
+            </a>
+          </p>
+      </video>
+      <playlist-component class="playlist" @clicked="playerSetUrl" v-bind:videos="videos"/>
+    </div>`
+
+});
 
 /* Playlist component */
 
@@ -68,6 +73,7 @@ Vue.component('playlist-component', {
       this.$emit('clicked', value);
     }
   },
+
   template: '<div><h2>Playlist</h2><item-component @clicked="onClickChild" v-for="item in videos" v-bind:item="item" v-bind:key="item.id"/></div>'
 });
 
@@ -95,10 +101,65 @@ Vue.component('item-component', {
   template: 
   `
     <div>
-      <button v-on:click="onClickButton(item)" v-bind:src="item | url">{{item | fullName}}</button>
+      <button class="button" v-on:click="onClickButton(item)" v-bind:src="item | url">{{item | fullName}}</button>
     </div>
     `
 });
+
+/* Chat */
+
+Vue.component('chat-component', {
+
+  data() {
+    return {
+      title: 'Chat - write message',
+      chat: '',
+      msg: []
+    }
+  },
+
+  methods: {
+    createMsg() {
+      if (this.msg.length > 2) {
+        this.msg.shift();
+        console.log('shifted!')
+      }
+      this.msg.push(this.chat);
+    }
+  },
+
+  template: `
+  <div>
+    <h4>{{title}}</h4>
+    <div class="msg"><p v-for="m in msg">{{m}}</p></div>
+    <form class="form" id="form" v-on:submit.prevent="createMsg">
+      <input type="text" v-model="chat" placeholder="Chat">
+      <button type="submit">Send!</button>
+    </form>
+  </div>
+  `
+});
+
+
+/* Title */
+
+Vue.component('title-component', {
+
+  data() {
+    return {
+      mainTitle: 'OUBS',
+      subTitle: 'video viewer'
+    }
+  },
+
+
+  template: `
+  <div class="title">
+    <h1>{{ mainTitle }}</h1>
+    <h2> {{ subTitle }}</h2>
+  </div>`
+})
+
 
 /* App */
 
@@ -118,5 +179,17 @@ const app = new Vue({
       }
     ]
   },
-  template: '<div><videoplayer-component v-bind:videos="videos"/></div>',
+  template: `
+  <div class="app">
+    <div class="header">
+      <title-component/>
+    </div>
+    <div class="main">
+      <videoplayer-component v-bind:videos="videos"/>
+    </div>
+    <div class="chat">
+      <chat-component/>
+    </div>
+  </div>
+  `
 })
